65b936ec2a2f8b66f9101803c8e661eceac6b72f
Ederson Prado <ederson.psales@gmail.com>
2025-07-05 16:39:25 -0300

    Concluida aula 6

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { TaskDto } from './task.dto';
 
 @Injectable()
@@ -8,5 +13,29 @@ export class TaskService {
   create(task: TaskDto) {
     this.tasks.push(task);
     console.log('Task created:', this.tasks);
+  }
+
+  findById(id: string): TaskDto | undefined {
+    const foundTask = this.tasks.filter((task) => task.id === id);
+    if (foundTask.length) {
+      return foundTask[0];
+    }
+
+    throw new HttpException(
+      `Task with id ${id} not found`,
+      HttpStatus.NOT_FOUND,
+    );
+  }
+
+  update(task: TaskDto): TaskDto {
+    const index = this.tasks.findIndex((t) => t.id === task.id);
+    if (index === -1) {
+      throw new HttpException(
+        `Task with id ${task.id} not found`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    this.tasks[index] = task;
+    return task;
   }
 }
